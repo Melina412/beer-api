07ee3c313cfb2035477c0ca6c226213d8ca14cbe
Melina412 <melina.schild@icloud.com>
2024-04-23 17:20:53 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Day56/', // git repo name
+  // base: '/Day56/', // git repo name
+
+  base: process.env.NODE_ENV === 'production' ? '/Day56/' : '/',
 });
